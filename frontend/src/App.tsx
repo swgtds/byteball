cb61f5be6187482d6b52366354765d9fd7c8e2f3
swgtds <swagatadas.sd@outlook.com>
2024-11-25 15:43:20 +0530

    update: App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 // src/App.tsx
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';  // Use Routes and Route for routing
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SportsSection from './components/SportsSection';
 import BorderGavaskarTrophyPage from './pages/BorderGavaskarTrophyPage'; // Your page component
+import StreamComingSoonPage from './pages/StreamComingSoonPage';  // The new page
 
 const App: React.FC = () => {
   const [selectedSport, setSelectedSport] = useState('Cricket');
@@ -14,6 +15,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<SportsSection sport={selectedSport} />} />
         <Route path="/border-gavaskar-trophy-2024" element={<BorderGavaskarTrophyPage />} />
+        <Route path="/coming-soon" element={<StreamComingSoonPage />} />
         {/* Other routes can be added here */}
       </Routes>
     </div>
