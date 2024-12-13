54bd4ef8fe75205602f8bd399d05389177317e5c
swgtds <swagatadas.sd@outlook.com>
2024-12-13 12:13:20 +0530

    added: new route paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import SportsSection from './components/SportsSection';
 import BorderGavaskarTrophyPage from './pages/BorderGavaskarTrophyPage';
 import StreamComingSoonPage from './pages/StreamComingSoonPage';
-import Header from './components/Header'; 
-
+import Header from './components/Header';
+import WatchVideoPage from './pages/WatchVideoPage';
 const App: React.FC = () => {
   const [selectedSport, setSelectedSport] = useState('Cricket');
 
   return (
     <div>
-      <Header /> 
+      <Header />
       <Routes>
         <Route path="/" element={<SportsSection sport={selectedSport} />} />
-        <Route
-          path="/border-gavaskar-trophy-2024"
-          element={<BorderGavaskarTrophyPage />}
-        />
+        <Route path="/border-gavaskar-trophy-2024" element={<BorderGavaskarTrophyPage />}/>
         <Route path="/coming-soon" element={<StreamComingSoonPage />} />
+        <Route path="/watch-video/:videoId" element={<WatchVideoPage />} />
+        <Route path="/watch-video" element={<WatchVideoPage />} />
       </Routes>
     </div>
   );
